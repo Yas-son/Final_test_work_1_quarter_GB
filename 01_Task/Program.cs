84197c3ed8f70d40940c29b66e13799f18d807fd
Yas-son <5trk@bk.ru>
2021-12-30 12:08:04 +0300

    Added code

--- 01_Task/Program.cs
+++ 01_Task/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] a = {-4,-3,-2,-1,0,1,2,3,4};
+
+for (int i = 0; i < a.Length; i++)
+{
+    if ((a[i] % 2) == 0)
+    {
+        Console.Write($"{a[i]} ");
+    }
+}
+
+//Dotnet run: -4 -2 0 2 4
